52b609615aff2b864de1779b10e63a31e3165329
Robert Luiz <robert.tiweb@gmail.com>
2020-08-22 09:46:12 -0300

    first commit

---
+++ Devboost.DroneDelivery.DomainService/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Devboost.DroneDelivery.DomainService
+{
+    public class Class1
+    {
+    }
+}
---
+++ Devboost.DroneDelivery.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Devboost.DroneDelivery.Repository
+{
+    public class Class1
+    {
+    }
+}
